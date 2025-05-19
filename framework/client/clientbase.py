c71487e9c741662751793b4db076a211de8aec41
Antoine HOUNSI <antoinehounsi3@gmail.com>
2025-05-19 03:21:39 +0200

    implementation of client et server base classes

---
+++ framework/client/clientbase.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+class Client(ABC):
+    """
+    This is the base class for a client.
+    For each type of algorithm, we would inherit from this class and implement methods
+    """
+    def __init__(self, client_id, train_loader, args, **kwargs):
+        self.client_id = client_id
+        self.train_loader = train_loader
+        self.optimizer = args.optimizer
+        self.criterion = args.criterion
+        self.scheduler = args.scheduler
+        self.max_epochs = args.max_epochs
+        self.nb_epochs = 0
+
+    @abstractmethod
+    def train(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def test(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+    @abstractmethod
+    def get_params(self):
+        pass
+
+    @abstractmethod
+    def set_params(self, **kwargs):
+        pass
---
+++ framework/server/serverbase.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+from framework.client.clientbase import Client
+
+
+class Server(ABC):
+    def __init__(self):
+        self.clients : list[Client] = []
+        self.clusters : dict[int, list] = dict()
+
+    @abstractmethod
+    def train(self):
+        pass
+
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+    @abstractmethod
+    def aggregate(self):
+        pass
+
+    @abstractmethod
+    def get_params(self):
+        pass
+
+
+
