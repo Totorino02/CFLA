c2d1e1e109d46f7dfb764e99318b4678980b6624
Antoine HOUNSI <antoinehounsi3@gmail.com>
2025-09-25 09:26:11 +0200

    modification of database import files

--- datasets/ag_news.py
+++ datasets/ag_news.py
@@ -7,7 +7,7 @@ path = "../data/AG_NEWS/"
 class AGNewsDataset:
     def __init__(self, path, ngrams=3):
         self.path = path
-        self.train_iter, self.test_iter = AG_NEWS(root = path, ngrams=ngrams)
+        self.train_iter, self.test_iter = AG_NEWS(root = path)#, ngrams=ngrams)
         
     def get_train_data(self):
         return pd.DataFrame(self.train_iter._data, columns=["targets", "data"])
