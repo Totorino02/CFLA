b0f6c139577e17440c4de93964c9eda717361a9b
Antoine HOUNSI <antoinehounsi3@gmail.com>
2026-03-03 04:14:09 +0100

    Readme + setup modification

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="CFLA",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "torch",
         "numpy",
         "tqdm",
+        "declearn @ git+https://gitlab.inria.fr/magnet/declearn/energy.git"
     ],
     author="HOUNSI Antoine",
     author_email="antoinehounsi3@gmail.com",
     description="A framework for federated learning algorithms",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Totorino02/CFLA/tree/main",
+    url="https://github.com/Totorino02/CFLA",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
